f492d00e344b926181069ac3ebba2671a76964bc
Denniii477 <130019753+Denniii477@users.noreply.github.com>
2023-05-07 23:07:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if(!empty($_SESSION['active'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/bulma.min.css">
-    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="login.css">
        <title>Login|Facturacion</title>
 </head>
 <body>
