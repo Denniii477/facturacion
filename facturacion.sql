82ca3e03f7b42c5aeda91ac00679117a175b17d7
Denniii477 <130019753+Denniii477@users.noreply.github.com>
2023-05-07 22:52:30 -0400

    Add files via upload

---
+++ facturacion.sql
@@ -0,0 +1,351 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-05-2023 a las 04:50:33
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `facturacion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idcliente` int(11) NOT NULL,
+  `nit` int(11) DEFAULT NULL,
+  `nombre` varchar(80) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `direccion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detallefactura`
+--
+
+CREATE TABLE `detallefactura` (
+  `correlativo` bigint(11) NOT NULL,
+  `nofactura` bigint(11) DEFAULT NULL,
+  `codproducto` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `preciototal` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_temp`
+--
+
+CREATE TABLE `detalle_temp` (
+  `correlativo` int(11) NOT NULL,
+  `nofactura` bigint(11) NOT NULL,
+  `codproducto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `preciototal` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entradas`
+--
+
+CREATE TABLE `entradas` (
+  `correlativo` int(11) NOT NULL,
+  `codproducto` int(11) NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT current_timestamp(),
+  `cantidad` int(11) NOT NULL,
+  `precio` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `nofactura` bigint(11) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `usuario` int(11) DEFAULT NULL,
+  `codcliente` int(11) DEFAULT NULL,
+  `totaltactura` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `codproducto` int(11) NOT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `proveedor` int(11) DEFAULT NULL,
+  `precio` decimal(10,2) DEFAULT NULL,
+  `existencia` int(11) DEFAULT NULL,
+  `foto` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `codproveedor` int(11) NOT NULL,
+  `proveedor` varchar(100) DEFAULT NULL,
+  `contacto` varchar(100) DEFAULT NULL,
+  `telefono` bigint(11) DEFAULT NULL,
+  `direccion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `idrol` int(11) NOT NULL,
+  `rol` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`idrol`, `rol`) VALUES
+(1, 'Administrador'),
+(2, 'Supervisor'),
+(3, 'Vendedor');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idusuario` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `usuario` varchar(15) DEFAULT NULL,
+  `clave` varchar(100) DEFAULT NULL,
+  `rol` int(11) DEFAULT NULL,
+  `estatus` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idusuario`, `nombre`, `correo`, `usuario`, `clave`, `rol`, `estatus`) VALUES
+(1, 'Dennis Choquecota', 'dennis@gmail.com', 'dennis', 'ae653896176d10c9be6e58feceb07505', 1, 1),
+(2, 'Limber Prado Atto', 'limber@gmail.com', 'limber', '25f9e794323b453885f5181f1b624d0b', 2, 1),
+(3, 'Francisco Obando', 'franchesco@gmail.com', 'francisco', '25f9e794323b453885f5181f1b624d0b', 2, 1),
+(4, 'Juan Matos', 'juan@gmail.com', 'sachiret', '25f9e794323b453885f5181f1b624d0b', 3, 1),
+(10, 'Administrador 1', 'admin1@gmail.com', 'admin1', 'ee7a9819f6948c41aaf61513f70487ae', 1, 1),
+(11, 'Supervisor 1', 'supervisor1@gmail.com', 'supervisor1', '25f9e794323b453885f5181f1b624d0b', 2, 1),
+(12, 'Vendedor1', 'vendedor1@gmail.com', 'vendedor1', '25f9e794323b453885f5181f1b624d0b', 3, 1),
+(13, 'prueba123', 'asdas@dasd.hgasdq', 'asdasdq', 'f5bb0c8de146c67b44babbf4e6584cc0', 3, 0),
+(14, 'Vendedor2', 'vendedor2@gmail.com', 'vendedor2', '25f9e794323b453885f5181f1b624d0b', 3, 1),
+(15, 'Vendedor3', 'vendedor3@gmail.com', 'vendedor3', '25f9e794323b453885f5181f1b624d0b', 3, 1),
+(16, 'Vendedor4', 'vendedor4@gmail.com', 'vendedor4', '25f9e794323b453885f5181f1b624d0b', 3, 1),
+(17, 'prueba123123', 'prueb@gmail.com123', 'prueba123', '25f9e794323b453885f5181f1b624d0b', 2, 0),
+(18, 'prueba125', 'prueba125@gmail.com', 'prueba125', 'f5bb0c8de146c67b44babbf4e6584cc0', 2, 0),
+(19, 'Vendedor6', 'vendedor6@gmail.com', 'vendedor6', '25f9e794323b453885f5181f1b624d0b', 3, 1),
+(20, 'asdas', 'ddasd@gmail.asdad', 'asdasd', 'a8f5f167f44f4964e6c998dee827110c', 2, 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idcliente`);
+
+--
+-- Indices de la tabla `detallefactura`
+--
+ALTER TABLE `detallefactura`
+  ADD PRIMARY KEY (`correlativo`),
+  ADD KEY `codproducto` (`codproducto`),
+  ADD KEY `nofactura` (`nofactura`);
+
+--
+-- Indices de la tabla `detalle_temp`
+--
+ALTER TABLE `detalle_temp`
+  ADD PRIMARY KEY (`correlativo`),
+  ADD KEY `nofactura` (`nofactura`),
+  ADD KEY `codproducto` (`codproducto`);
+
+--
+-- Indices de la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  ADD PRIMARY KEY (`correlativo`),
+  ADD KEY `codproducto` (`codproducto`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`nofactura`),
+  ADD KEY `usuario` (`usuario`),
+  ADD KEY `codcliente` (`codcliente`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`codproducto`),
+  ADD KEY `proveedor` (`proveedor`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`codproveedor`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`idrol`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idusuario`),
+  ADD KEY `rol` (`rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detallefactura`
+--
+ALTER TABLE `detallefactura`
+  MODIFY `correlativo` bigint(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_temp`
+--
+ALTER TABLE `detalle_temp`
+  MODIFY `correlativo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  MODIFY `correlativo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `nofactura` bigint(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `codproducto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `codproveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `idrol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detallefactura`
+--
+ALTER TABLE `detallefactura`
+  ADD CONSTRAINT `detallefactura_ibfk_1` FOREIGN KEY (`nofactura`) REFERENCES `factura` (`nofactura`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detallefactura_ibfk_2` FOREIGN KEY (`codproducto`) REFERENCES `producto` (`codproducto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `detalle_temp`
+--
+ALTER TABLE `detalle_temp`
+  ADD CONSTRAINT `detalle_temp_ibfk_1` FOREIGN KEY (`nofactura`) REFERENCES `factura` (`nofactura`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detalle_temp_ibfk_2` FOREIGN KEY (`codproducto`) REFERENCES `producto` (`codproducto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  ADD CONSTRAINT `entradas_ibfk_1` FOREIGN KEY (`codproducto`) REFERENCES `producto` (`codproducto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `factura_ibfk_2` FOREIGN KEY (`codcliente`) REFERENCES `cliente` (`idcliente`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`proveedor`) REFERENCES `proveedor` (`codproveedor`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`rol`) REFERENCES `rol` (`idrol`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
